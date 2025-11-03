0b03ea91eedab612f5261175a2f691a90d96bc9a
baabaablacksheep2102 <harishgprabu@gmail.com>
2025-11-03 20:22:20 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+﻿/** @type {import("tailwindcss").Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3b82f6",
+        secondary: "#10b981",
+        danger: "#ef4444",
+      }
+    },
+  },
+  plugins: [],
+}
